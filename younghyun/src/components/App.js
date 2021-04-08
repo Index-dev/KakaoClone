1ae7cad74d669440de4c3d2962f4ef51b5598c47
Eum <opwer032@naver.com>
2021-04-08 09:05:07 +0900

    refactor: Remove useless logic

--- younghyun/src/components/App.js
+++ younghyun/src/components/App.js
@@ -1,19 +1,13 @@
-import { useMediaQuery } from "react-responsive";
 import Aligner from "components/responsive/centerAligner";
 import GlobalStyle from "components/responsive/globalStyles";
-import MobileStyle from "components/responsive/mobileStyles";
 import Router from "components/router";
 
 const MAX_WIDTH = "500px";
 
 function App() {
-    const isMobile = useMediaQuery({
-        query: `(max-width: ${MAX_WIDTH})`,
-    });
-
     return (
         <>
-            {isMobile ? <MobileStyle /> : <GlobalStyle />}
+            <GlobalStyle />
             <Aligner maxWidth={MAX_WIDTH}>
                 <Router />
             </Aligner>
