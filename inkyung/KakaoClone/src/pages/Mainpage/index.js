0489bc16347a53d85266de701c0a23e1ad5e2f9e
inkyung <inky94@naver.com>
2021-04-09 00:27:29 +0900

    add: main page

--- inkyung/KakaoClone/src/pages/Mainpage/index.js
+++ inkyung/KakaoClone/src/pages/Mainpage/index.js
@@ -1,11 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+import Nav from '../../components/Main/Nav';
+import Me from '../../components/Main/Me';
+import Friends from '../../components/Main/Friends';
+import Topbar from "../../components/Main/Topbar"
 
 class Mainpage extends React.Component{
     render(){
         return(
-            <div>메인페이지</div>
+            <Frame>
+                <Topbar></Topbar>
+                <Title>우리는 칭구칭구!</Title>
+                <List>
+                    <Me></Me>
+                    <Friends></Friends>
+                </List>
+                <Nav></Nav>
+            </Frame>
         )
     }
 }
+
+const Frame = styled.div`
+    height: 100vh;
+    display: grid;
+    align-content: center;
+    grid-template-rows: 5% 5% 80% 10%;
+    grid-template-areas:
+        "topbar"
+        "title"
+        "list"
+        "nav"
+    ;
+    
+`
+
+const Title = styled.div`
+    display: flex; 
+    align-items: center; 
+    justify-content: left; 
+    font-weight: bold;
+    font-size: x-large;
+`
+
+const List = styled.div`
+    grid-area: list;
+    padding-left: 5%;
+    display: grid;
+    grid-template-rows: 15%;
+    grid-template-areas: 
+        "profile"
+        "friendlist"
+    ;
+`;
 
 export default Mainpage;
