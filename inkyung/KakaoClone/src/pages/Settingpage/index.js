e342bbfce0e3fa68ad93f2edbe9edb6890de0db0
inkyung <inky94@naver.com>
2021-04-10 01:03:46 +0900

    modify: css, components

--- inkyung/KakaoClone/src/pages/Settingpage/index.js
+++ inkyung/KakaoClone/src/pages/Settingpage/index.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import Topbar from "../../components/Main/Topbar";
 import Nav from "../../components/Main/Nav";
 import Info from "../../components/Sets/Info";
+import {FcSettings} from "react-icons/fc";
 
 class Settingpage extends React.Component{
     render(){
         return(
             <Frame>
                 <Topbar></Topbar>
+                <Title>설정<Searchicon/></Title>
                 <Info>
                 </Info>
                 <Nav></Nav>
@@ -20,12 +22,28 @@ class Settingpage extends React.Component{
 const Frame = styled.div`
     height: 100vh;
     display: grid;
-    grid-template-rows: 5% 85% 10%;
+    grid-template-rows: 5% 10% 75% 10%;
     grid-template-areas:
         "topbar"
+        "title"
         "list"
         "nav"
     ;
 `;
+
+const Title = styled.div`
+    grid-area: title;
+    padding-left: 10%;
+    font-size: xx-large;
+    display: flex; 
+    align-items: center; 
+    background-color: #ffe6e6;;
+`
+
+
+export const Searchicon = styled(FcSettings)`
+    font-size: xx-large;
+    padding-left: 1%;
+`
 
 export default Settingpage;
