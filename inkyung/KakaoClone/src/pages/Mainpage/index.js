2e1446073d442ae332b5a7e97fc27e202d8f064c
inkyung <inky94@naver.com>
2021-04-10 01:02:05 +0900

    style: css update

--- inkyung/KakaoClone/src/pages/Mainpage/index.js
+++ inkyung/KakaoClone/src/pages/Mainpage/index.js
@@ -4,13 +4,14 @@ import Nav from '../../components/Main/Nav';
 import Me from '../../components/Main/Me';
 import Friends from '../../components/Main/Friends';
 import Topbar from "../../components/Main/Topbar"
+import {FcLike} from "react-icons/fc";
 
 class Mainpage extends React.Component{
     render(){
         return(
             <Frame>
                 <Topbar></Topbar>
-                <Title>우리는 칭구칭구!</Title>
+                <Title>친구<Like/></Title>
                 <List>
                     <Me></Me>
                     <Friends></Friends>
@@ -25,22 +26,22 @@ const Frame = styled.div`
     height: 100vh;
     display: grid;
     align-content: center;
-    grid-template-rows: 5% 5% 80% 10%;
+    grid-template-rows: 5% 10% 75% 10%;
     grid-template-areas:
         "topbar"
         "title"
         "list"
         "nav"
     ;
-    
 `
 
 const Title = styled.div`
+    grid-area: title;
+    padding-left: 10%;
+    font-size: xx-large;
     display: flex; 
     align-items: center; 
-    justify-content: left; 
-    font-weight: bold;
-    font-size: x-large;
+    background-color: #ffe6e6;
 `
 
 const List = styled.div`
@@ -53,5 +54,10 @@ const List = styled.div`
         "friendlist"
     ;
 `;
+
+export const Like = styled(FcLike)`
+    font-size: xx-large;
+    padding-left: 1%;
+`
 
 export default Mainpage;
