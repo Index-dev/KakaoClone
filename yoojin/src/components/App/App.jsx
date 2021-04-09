38e814dd88cbb630d8198b4d6ca365511e6a9454
daisy-yoojin <daisy.park95@gmail.com>
2021-04-09 16:54:21 +0900

    modify: App.jsx change name of Topbar(->TopBar)

--- yoojin/src/components/App/App.jsx
+++ yoojin/src/components/App/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "../../pages/LoginPage";
 import Main from "../../pages/MainPage";
-import Topbar from "../Navbar/Topbar";
+import Topbar from "../Navbar/TopBar";
 
 const App = () => {
   return (
