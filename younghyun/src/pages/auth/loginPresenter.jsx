da6a7691e0b9ec3589d06ee4f0f8992b99c90c3d
Eum <opwer032@naver.com>
2021-04-09 22:45:02 +0900

    feat: Set layout of loginform

--- younghyun/src/pages/auth/loginPresenter.jsx
+++ younghyun/src/pages/auth/loginPresenter.jsx
@@ -1,3 +1,4 @@
+import { useRef, useLayoutEffect, useEffect } from "react";
 import styled from "styled-components";
 import ruinedCity from "assets/image/ruinedCity.jpg";
 
@@ -5,7 +6,8 @@ const Container = styled.div`
     width: 100%;
     height: 100%;
     position: relative;
-    display: grid;
+    background-color: rgba(0, 0, 0, 1);
+    z-index: -1;
 `;
 
 const BackgroundImage = styled.div`
@@ -16,19 +18,68 @@ const BackgroundImage = styled.div`
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
+    opacity: 70%;
     z-index: -1;
 `;
 
 const LoginInfo = styled.div`
-    width: 60%;
-    border: 1px solid white;
+    min-width: 320px;
+    width: 60vw;
+    max-width: 360px;
+    min-height: 512px;
+    height: 97.08vw;
+    max-height: 582px;
+
+    position: absolute;
+    top: 38%;
+    left: 50%;
+
+    background-color: rgba(0, 0, 0, 0.7);
+    transform: translateX(-50%) translateY(-38%);
+    z-index: 1;
 `;
 
 function LoginPresenter() {
+    const containerRef = useRef();
+    const divRef = useRef();
+
+    // useEffect(() => {
+    //     screenResize();
+    // });
+    // useLayoutEffect(() => {
+    //     window.addEventListener("resize", screenResize);
+    // });
+
+    // function screenResize() {
+    //     const blockHeight = calcHeight();
+    //     const surplusHeight = calcSurplusHeight(blockHeight);
+    //     if (divRef !== undefined) {
+    //         console.log(surplusHeight, surplusHeight / 2.618);
+    //         divRef.current.style.top = `${surplusHeight / 2.618}px`;
+    //     }
+    // }
+    // function calcSurplusHeight(blockHeight) {
+    //     let result = 0;
+    //     if (containerRef !== undefined) {
+    //         result = containerRef.current.clientHeight - blockHeight;
+    //     }
+    //     return result;
+    // }
+    // function calcHeight() {
+    //     let result = 0;
+    //     if (containerRef !== undefined) {
+    //         result = containerRef.current.clientWidth * 0.9708;
+    //         console.log(containerRef.current.clientHeight / 2);
+    //         if (result < 512) return 512;
+    //         else if (result > 582) return 582;
+    //     }
+    //     return result;
+    // }
+
     return (
-        <Container>
+        <Container ref={containerRef}>
             <BackgroundImage />
-            <LoginInfo />
+            <LoginInfo ref={divRef}></LoginInfo>
         </Container>
     );
 }
