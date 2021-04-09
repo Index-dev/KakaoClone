654bab93311312d133fdce373639de441e07e95a
daisy-yoojin <daisy.park95@gmail.com>
2021-04-09 17:02:07 +0900

    add: work for Profile(undo)

---
+++ yoojin/src/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import user from '../../img/users';
+const Profile =()=> {
+    return (
+        <div>
+            <img src={user}/>
+            <div>이름</div>
+            <div>message</div>
+        </div>
+    )
+}
+
+export default Profile
--- yoojin/src/pages/MainPage/index.js
+++ yoojin/src/pages/MainPage/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import BottomBar from '../../components/Navbar/BottomBar';
 import TopBar from '../../components/Navbar/TopBar';
+import Profile from '../../components/Profile/Profile';
 import './style.css';
 const Main=()=> {
     return (
         <div className="Frame">
             <TopBar/>
             <div className="main__content">
-                <div className="content"></div>
+                <div className="content">
+                    <Profile/>
+                </div>
             <BottomBar/>
             </div>
         </div>
