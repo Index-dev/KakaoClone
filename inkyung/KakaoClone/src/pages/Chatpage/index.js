2db525735e7c3cf8bea8a96f4b075f697628ce41
inkyung <inky94@naver.com>
2021-04-11 14:15:44 +0900

    modify: css, grid

--- inkyung/KakaoClone/src/pages/Chatpage/index.js
+++ inkyung/KakaoClone/src/pages/Chatpage/index.js
@@ -3,14 +3,28 @@ import styled from "styled-components";
 import Topbar from "../../components/Main/Topbar";
 import Chat from "../../components/Chats/Chat";
 import Nav from "../../components/Main/Nav";
-import {FcVoicePresentation} from "react-icons/fc";
+import {BiSearch,BiMessageRoundedAdd} from "react-icons/bi";
+import {IoSettingsOutline,IoMusicalNotesOutline} from "react-icons/io5";
+import Advertisement from "../../Photos/Ad.jpg";
 
 class Chatpage extends React.Component{
     render(){
         return(
             <Frame>
                 <Topbar></Topbar>
-                <Title>채팅<Chaticon/></Title>
+                <Title>
+                    <Text>채팅</Text>
+                    <Icons>
+                        <SearchI/>
+                        <AddI/>
+                        <MusicI/>
+                        <SettingI/>
+                    </Icons>
+                </Title>
+                <Ad>
+                    <Img src={Advertisement}/>
+                </Ad>
+                <div/>
                 <Chat></Chat>
                 <Nav></Nav>
             </Frame>
@@ -21,10 +35,12 @@ class Chatpage extends React.Component{
 const Frame = styled.div`
     height: 100vh;
     display: grid;
-    grid-template-rows: 5% 10% 75% 10%;
+    grid-template-rows: 5% 7% 10% 1% 67% 10%;
     grid-template-areas:
         "topbar"
         "title"
+        "ad"
+        "."
         "list"
         "nav"
     ;
@@ -33,16 +49,40 @@ const Frame = styled.div`
 
 const Title = styled.div`
     grid-area: title;
-    padding-left: 10%;
-    font-size: xx-large;
+    padding-left: 5%;
+    padding-right: 3%;
+    font-size: x-large;
     display: flex; 
-    align-items: center; 
-    background-color: #ffe6e6;
+    align-items: center;
+    justify-content: space-between;
 `
 
-export const Chaticon = styled(FcVoicePresentation)`
-    font-size: xx-large;
-    padding-left: 1%;
+const Text = styled.div``
+
+const Icons = styled.div`
+    display: flex;
+    width: 6em;
+    justify-content: space-evenly;
 `
+
+const Ad = styled.div`
+    padding-left: 5%;
+    padding-right: 5%;
+`
+
+const Img = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: .3em;
+`
+
+export const SearchI = styled(BiSearch)``
+
+export const SettingI = styled(IoSettingsOutline)``
+
+export const MusicI = styled(IoMusicalNotesOutline)``
+
+export const AddI = styled(BiMessageRoundedAdd)``
 
 export default Chatpage;
