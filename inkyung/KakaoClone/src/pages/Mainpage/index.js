f787f51b5ed4643923c32d8970425d3786e84f22
inkyung <inky94@naver.com>
2021-04-11 14:18:35 +0900

    modify: grid, style, components

--- inkyung/KakaoClone/src/pages/Mainpage/index.js
+++ inkyung/KakaoClone/src/pages/Mainpage/index.js
@@ -3,15 +3,25 @@ import styled from "styled-components";
 import Nav from '../../components/Main/Nav';
 import Me from '../../components/Main/Me';
 import Friends from '../../components/Main/Friends';
-import Topbar from "../../components/Main/Topbar"
-import {FcLike} from "react-icons/fc";
+import Topbar from "../../components/Main/Topbar";
+import {BiSearch} from "react-icons/bi";
+import {IoSettingsOutline,IoMusicalNotesOutline} from "react-icons/io5";
+import { RiUserAddLine } from "react-icons/ri";
 
 class Mainpage extends React.Component{
     render(){
         return(
             <Frame>
                 <Topbar></Topbar>
-                <Title>친구<Like/></Title>
+                <Title>
+                    <Text>친구</Text>
+                    <Icons>
+                        <SearchI/>
+                        <AddI/>
+                        <MusicI/>
+                        <SettingI/>
+                    </Icons>
+                </Title>
                 <List>
                     <Me></Me>
                     <Friends></Friends>
@@ -26,7 +36,7 @@ const Frame = styled.div`
     height: 100vh;
     display: grid;
     align-content: center;
-    grid-template-rows: 5% 10% 75% 10%;
+    grid-template-rows: 5% 7% 78% 10%;
     grid-template-areas:
         "topbar"
         "title"
@@ -37,27 +47,39 @@ const Frame = styled.div`
 
 const Title = styled.div`
     grid-area: title;
-    padding-left: 10%;
-    font-size: xx-large;
+    padding-left: 5%;
+    padding-right: 3%;
+    font-size: x-large;
     display: flex; 
-    align-items: center; 
-    background-color: #ffe6e6;
+    align-items: center;
+    justify-content: space-between;
 `
+
+const Text = styled.div``
 
 const List = styled.div`
     grid-area: list;
     padding-left: 5%;
     display: grid;
-    grid-template-rows: 15%;
+    grid-template-rows: 10% 68%;
     grid-template-areas: 
         "profile"
         "friendlist"
     ;
 `;
 
-export const Like = styled(FcLike)`
-    font-size: xx-large;
-    padding-left: 1%;
+const Icons = styled.div`
+    display: flex;
+    width: 6em;
+    justify-content: space-evenly;
 `
+
+export const SearchI = styled(BiSearch)``
+
+export const SettingI = styled(IoSettingsOutline)``
+
+export const MusicI = styled(IoMusicalNotesOutline)``
+
+export const AddI = styled(RiUserAddLine)``
 
 export default Mainpage;
