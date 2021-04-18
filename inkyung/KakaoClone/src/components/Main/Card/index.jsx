4d63289ccfdf5f5473ef4eb849ee88b32e64c6d5
inkyung <inky94@naver.com>
2021-04-18 14:14:58 +0900

    add: create file for card

---
+++ inkyung/KakaoClone/src/components/Main/Card/index.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+
+const Card = ({ post }) => {
+    const { Friend_ID, NAME, PImg, PMusic, PMessage, Like } = post;
+    return(
+        <Fprofile>
+            <Fphoto>
+                <PButton>
+                <IMG src={PImg}></IMG>
+                </PButton>
+            </Fphoto>
+            <InfoF>
+                <Fname>{NAME}</Fname>
+                <Message>{PMessage}</Message>
+            </InfoF>
+            <MDiv>
+                <Music>{PMusic}</Music>
+            </MDiv>
+        </Fprofile>
+    )
+}
+
+const Fprofile = styled.div`
+    height: 60px;
+    display: grid; 
+    grid-template-columns: 18% 20% auto;
+    grid-template-areas: "img name msg";
+    &:hover {
+        cursor: pointer;
+        background-color: #dbdbdb;
+    }
+`
+
+const Fphoto = styled.div`
+    grid-area: img;
+    display: flex; 
+    align-items: center;
+`
+
+const IMG = styled.img`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    width: 2.2em;
+    height: 2.2em;
+    object-fit: cover;
+    border-radius: .8em;
+    border: 1px solid #ededed;
+`
+
+const PButton = styled.div`
+
+`
+
+const InfoF = styled.div`
+    display: flex; 
+    flex-direction: column;
+    justify-content: space-evenly;
+`
+
+const Fname = styled.div`  
+    grid-area: name;
+    display: flex; 
+    align-items: center; 
+    font-weight: 550;
+`
+
+const Message = styled.div`
+    font-size: small;
+    font-weight: 250;
+`
+
+const MDiv = styled.div`
+    grid-area: msg;
+    display: flex; 
+    align-items: center; 
+    justify-content: flex-end;
+`
+const Music = styled.div`
+    font-size: small;
+    font-weight: 100;
+    border: 1px solid gray;
+    border-radius: 2em;
+    padding-top: 1.5%;
+    padding-bottom: 1.5%;
+    padding-left: 3.5%;
+    padding-right: 3.5%;
+    border-color: #2ced1f;
+`
+
+
+export default Card;
