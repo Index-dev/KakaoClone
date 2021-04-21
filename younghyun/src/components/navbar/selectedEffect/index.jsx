a8cc2c6f083db33494e7d9f18438e8a8e28fe131
EumYoungHyun <opwer032@naver.com>
2021-04-22 00:52:32 +0900

    feat: Make selected navbar effect

---
+++ younghyun/src/components/navbar/selectedEffect/index.jsx
@@ -0,0 +1,84 @@
+import { useEffect, createRef } from "react";
+import styled from "styled-components";
+
+function SelectedEffect() {
+  const canvasRef = createRef();
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    ctx.width = canvas.offsetWidth;
+    ctx.height = canvas.offsetHeight;
+
+    class Circle {
+      constructor(x, y, dx, dy, radius) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+
+        let colorArray = ["#663300", "#ff8000", "#ffff00"];
+
+        this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+      }
+
+      draw = () => {
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+        this.update();
+      };
+
+      update = () => {
+        if (
+          this.x + this.radius / 2 >= canvas.offsetWidth ||
+          this.x - this.radius / 2 <= 0
+        ) {
+          this.dx = -this.dx;
+        }
+        if (
+          this.y + this.radius / 2 >= canvas.offsetHeight ||
+          this.y - this.radius / 2 <= 0
+        ) {
+          this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+      };
+    }
+    let circleArray = [];
+
+    for (let i = 0; i < 80; i++) {
+      let radius = Math.random() * 10 + 4;
+      let x = Math.random() * canvas.offsetWidth;
+      let dx = (Math.random() - 0.2) * 1;
+      let y = Math.random() * canvas.offsetHeight;
+      let dy = (Math.random() - 0.2) * 1;
+      circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+    function animate() {
+      requestAnimationFrame(animate);
+      ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+
+      circleArray.forEach((circle) => {
+        circle.draw();
+      });
+    }
+
+    animate();
+  }, [canvasRef]);
+
+  return <Canvas ref={canvasRef} className="slideEffect" />;
+}
+
+export default SelectedEffect;
+
+const Canvas = styled.canvas`
+  position: absolute;
+  z-index: 0;
+`;
