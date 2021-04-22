f286d43aaefb898df04c711237df9b8bad5ae318
inkyung <inky94@naver.com>
2021-04-23 00:52:12 +0900

    update: add new functions, modify state values

--- inkyung/KakaoClone/src/components/Chats/Chat.js
+++ inkyung/KakaoClone/src/components/Chats/Chat.js
@@ -11,7 +11,8 @@ class Info extends React.Component{
         showChat: false,
         friends: [],
         chats: [],
-        chatsContent: []
+        chatsContent: [],
+        new: []
     };
 
     componentDidMount() {
@@ -19,33 +20,48 @@ class Info extends React.Component{
         friendStore.getFriends(0,3);
         chatStore.getChats(0,2);
         this.setState({
-            chats: chatStore.returnChats,
             friends: friendStore.returnFriends
-
+        });
+        let Clength = chatStore.length;
+        let Flength = friendStore.length;
+        friendStore.concatReset();
+       for(let i=0;i<Flength;i++){
+        for(let j=0;j<Clength;j++){
+            if(chatStore.returnChats[j].Friend_ID === friendStore.returnFriends[i].Friend_ID) {
+              friendStore.concatItems(chatStore.returnChats[j], friendStore.returnFriends[i]);   
+            }
+        }    
+       }
+       this.setState({
+            chats: friendStore.combines
         });
     }
 
     toggleChatting = (e) => {
-        const { chatStore } = this.props;
-        chatStore.getChats(e,e+1);
+        const { friendStore } = this.props;
+        friendStore.getOne(e,friendStore.combines);
         this.setState({
             showChat: !this.state.showChat,
-            chatsContent: chatStore.returnChats[0]
+            chatsContent: friendStore.returnFriendInfo
         });
-
+        
       }
 
     render(){
-        const{chats,friends,chatsContent} = this.state;
+        const{chats, chatsContent} = this.state;
         return(
             <Out>
             <Frame>
                 {chats.map((item, index) => (
-                        <Card key={index} post={item} value={friends} onToggle={this.toggleChatting}></Card> 
+                        <Card key={index} post={item}onToggle={this.toggleChatting}></Card> 
                     ))}
             </Frame>
+            
             {this.state.showChat?(
-                <Contents value={chatsContent} cancelChat={this.toggleChatting.bind(this)}/>
+                <div>{chatsContent.map((item, index) => (
+                    <Contents key={index} post={item} cancelChat={this.toggleChatting.bind(this)}/>
+                ))}</div>
+                
             ) : null}
             </Out>
         )
