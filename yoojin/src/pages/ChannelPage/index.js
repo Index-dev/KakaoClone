a414d0aac3ab43c04740457d96ef2d59b82fe950
daisy_yoojin <daisy.park95@gmail.com>
2021-04-22 20:46:30 +0900

    chore: channel page navbar

--- yoojin/src/pages/ChannelPage/index.js
+++ yoojin/src/pages/ChannelPage/index.js
@@ -4,31 +4,34 @@ import TopBar from "../../components/Navbar/TopBar";
 import BottomMenu from "../../components/Navbar/BottomMenu";
 import menu from "../../asset/img/list.png";
 function ChannelPage() {
+  const menuItems = [
+    "코로나19",
+    "톡업데이트",
+    "뉴스",
+    "카카오tv",
+    "쇼핑",
+    "Fun",
+    "연예",
+    "스포츠",
+    "공연",
+    "뮤직"
+  ];
   return (
     <div className="Frame">
       <TopBar />
       <div className="main__content">
-        {/* top menu 모양이 다름 */}
-        {/* <TopMenu tabTitle="채널" /> */}
         <div className="channel__top">
-          <ul>
-            <li>코로나19</li>
-            <li>톡업데이트</li>
-            <li>#뉴스</li>
-            <li>카카오TV</li>
-            <li>쇼핑</li>
-            <li>Fun</li>
-            <li>연예</li>
-            <li>스포츠</li>
-            <li>공연</li>
-            <li>뮤직</li>
-          </ul>
+          <div className="channel__menu">
+            {menuItems.map((item, i) => {
+              return <div>{item}</div>;
+            })}
+          </div>
           {/* 임시 사용 */}
           <div>
             <img src={menu} width="20px" alt="" />
           </div>
         </div>
-        <div className="content">내용</div>
+        <div className="content"></div>
         <BottomMenu />
       </div>
     </div>
