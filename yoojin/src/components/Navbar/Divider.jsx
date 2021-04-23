05958c6616e5ebc4bb806d27267008893fb43522
daisy_yoojin <daisy.park95@gmail.com>
2021-04-24 08:53:33 +0900

    feat: Add more profiles and refact divider

--- yoojin/src/components/Navbar/Divider.jsx
+++ yoojin/src/components/Navbar/Divider.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Divider.scss";
 import down from "../../asset/img/arrow-down.png";
-
-const Divider = ({ dividerTitle }) => {
+import up from "../../asset/img/arrow-up.png";
+import ProfileListItem from "../../components/Profile/ProfileListItem";
+const Divider = ({ dividerTitle, divState }) => {
+  const [divFrnd, setDivFrnd] = useState(true);
+  const stateHandler = () => {
+    setDivFrnd(!divFrnd);
+  };
   return (
-    <div className="divider__frame">
-      <div className="divider__text">{dividerTitle}</div>
-      <div className="divider__dirImg">
-        <img src={down} alt="" />
+    <div>
+      <div className="divider__frame">
+        <div className="divider__text">{dividerTitle}</div>
+        <div className="divider__dirImg">
+          {divFrnd ? (
+            <img src={up} alt="" onClick={stateHandler} />
+          ) : (
+            <img src={down} alt="" onClick={stateHandler} />
+          )}
+        </div>
       </div>
+      {divFrnd ? (
+        <div>
+          <ProfileListItem />
+          <ProfileListItem />
+          <ProfileListItem />
+          <ProfileListItem />
+          <ProfileListItem />
+        </div>
+      ) : null}
     </div>
   );
 };
