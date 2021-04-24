9c05c9337b7d526ca16add97a1024cf08c3bdb73
EumYoungHyun <opwer032@naver.com>
2021-04-24 09:35:47 +0900

    feat: Add chat input bar

--- younghyun/src/components/chat/detail/chat.jsx
+++ younghyun/src/components/chat/detail/chat.jsx
@@ -23,6 +23,11 @@ function Chat() {
           <TimeStamp>12시 1분</TimeStamp>
         </LeftTalk>
       </TalkContainer>
+      <InputBar>
+        <InputContainer>
+          <Input />
+        </InputContainer>
+      </InputBar>
     </Container>
   );
 }
@@ -31,7 +36,9 @@ export default Chat;
 
 const Container = styled.div`
   max-width: 100%;
+  height: 80%;
   max-height: 100%;
+  position: relative;
   padding: 100px 20px 50px 20px;
 `;
 
@@ -73,4 +80,35 @@ const TimeStamp = styled.div`
   align-items: flex-end;
   font-size: 0.8em;
   padding: 0 0.3rem 0.7rem 0.3rem;
+`;
+
+const InputBar = styled.div`
+  width: 100%;
+  height: 50px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(247, 203, 0, 0.7);
+`;
+
+const InputContainer = styled.div`
+  width: 80%;
+  height: 80%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid rgba(247, 203, 0);
+  border-radius: 50px;
+`;
+const Input = styled.input`
+  width: 80%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0);
+
+  outline: none;
+  border: none;
 `;
